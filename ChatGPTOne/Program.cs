c00213d6dcd907032df9f5194b9c602bb8660b2a
gcr2000e <a22409619@alunos.ulht.pt>
2025-02-26 21:13:48 +0000

    Add full ChatGPTOne project

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -10,12 +10,34 @@ namespace ChatGPTOne
 
             do
             {
-               Console.Write("");
+               Console.Write("Place your question: ");
+               question = Console.ReadLine();
                switch(question)
                {
+                  case "How are you?":
+                   response = "I'm fine, thank you";
+                   break;
 
+                  case "Are you an AI?":
+                   response = "No, I'm not";
+                   break;
+                  
+                  case "Can you do math?":
+                   response = "Not yet, my dev didn't coded me to do that yet";
+                   break;
+                  
+                  case "Are you smart enough?":
+                   response = "Nope, I'm dumb as a stone";
+                   break;
+                  
+                  default:
+                   response = "Sorry I'm not able to answer that question";
+                   break;
                }
-            }while(question != "EXIT")
+
+               Console.WriteLine(response);
+            }
+            while(question != "EXIT");
         }
     }
 }
