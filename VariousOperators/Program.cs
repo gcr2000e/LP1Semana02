993c2ccc003bc686c743ba9d65e262615465539d
gcr2000e <a22409619@alunos.ulht.pt>
2025-02-26 18:21:45 +0000

    Add VariousOperators project

---
+++ VariousOperators/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace VariousOperators
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Insire um número inteiro não negativo: ");
+            string n = Console.ReadLine();
+
+            byte num = byte.Parse(n);
+
+            Console.WriteLine(num / 2);
+            Console.WriteLine(num << 3);
+            Console.WriteLine(num ^ 6);
+            bool b;
+
+            if (num > 10)
+            {
+                b = true;
+                Console.WriteLine($"{b}");
+            }
+            else
+            {
+                b = false;
+                Console.WriteLine($"{b}");
+            }
+
+
+        }
+    }
+}
