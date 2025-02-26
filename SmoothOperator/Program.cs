56157a659288e56d5cdc3f1e85bed1508dfaacc2
gcr2000e <a22409619@alunos.ulht.pt>
2025-02-26 18:02:51 +0000

    Add SmoothOperator project

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Insere um número inteiro: ");
+            string n = Console.ReadLine();
+
+            sbyte num = sbyte.Parse(n);
+
+            Console.WriteLine(--num);
+            Console.WriteLine(++num);
+        }
+    }
+}
